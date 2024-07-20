74e4c88886c986513ae40e7ac02b00935c658965
Etienne Jacquot <etiennej@upenn.edu>
2024-07-19 22:52:36 -0400

    code: sizing more for mobile

--- src/Components/Buttafly.js
+++ src/Components/Buttafly.js
@@ -9,10 +9,9 @@ class Buttafly extends React.Component {
             <div class='App'>
                 <div class="container" style={{ backgroundImage: "url(https://img.saysdont.com/images/butterfly_promo.gif)",
                                                 backgroundPosition: 'bottom',
-                                                backgroundSize: '165%',
+                                                backgroundSize: '100%',
                                                 backgroundRepeat: 'no-repeat',}}>
                     <br />
-                    <br />                   
                     <p><span class="before-colon"><a class="menu_links_preface">See more here:</a></span> <a href="https://linktr.ee/blackbuttafly" target="_blank" class="menu_links">linktr.ee/blackbuttafly</a></p>
                     <p><span class="before-colon"><a class="menu_links_preface">ig (BUTTAFLY):</a></span> <a href="https://www.instagram.com/blackbuttaflyofficial" target="_blank" class="menu_links">@blackbuttaflyofficial</a></p>
                     <p><span class="before-colon"><a class="menu_links_preface">ig (NU SCHOOL):</a></span> <a href="https://www.instagram.com/thenuschoolcollective" target="_blank" class="menu_links">@thenuschoolcollective</a></p>
